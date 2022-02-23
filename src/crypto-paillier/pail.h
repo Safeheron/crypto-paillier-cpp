8972f703140f39d3a99425d5f0c8963de8d00025
何剑虹 <hejh1500@gmail.com>
2022-02-23 16:36:04 +0800

    First Commit

---
+++ src/crypto-paillier/pail.h
@@ -0,0 +1,43 @@
+//
+// Created by 何剑虹 on 2020/10/26.
+//
+
+#ifndef CPP_MPC_PAILLIAR_H
+#define CPP_MPC_PAILLIAR_H
+
+#include "crypto-bn/bn.h"
+#include "paillier.pb.h"
+
+#include "pail_pubkey.h"
+#include "pail_privkey.h"
+
+
+namespace safeheron{
+namespace pail {
+
+void CreateKeyPair(PailPrivKey &priv, PailPubKey &pub, int key_bits);
+
+void CreateKeyPair1024(PailPrivKey &priv, PailPubKey &pub);
+
+void CreateKeyPair2048(PailPrivKey &priv, PailPubKey &pub);
+
+void CreateKeyPair3072(PailPrivKey &priv, PailPubKey &pub);
+
+void CreateKeyPair4096(PailPrivKey &priv, PailPubKey &pub);
+
+PailPrivKey CreatePailPrivKey(const std::string& lambda_hex, const std::string& mu_hex, const std::string& n_hex, const std::string& n_sqr_hex,
+                              const std::string& p_hex, const std::string& q_hex,
+                              const std::string& p_sqr_hex, const std::string& q_sqr_hex,
+                              const std::string& p_minus_1_hex, const std::string& q_minus_1_hex,
+                              const std::string& hp_hex, const std::string& hq_hex,
+                              const std::string& q_inv_p_hex, const std::string& p_inv_q_hex);
+
+PailPrivKey CreatePailPrivKey(const std::string& lambda_hex, const std::string& mu_hex, const std::string& n_hex);
+
+PailPubKey CreatePailPubKey(const std::string& n_hex, const std::string& g_hex);
+
+};
+};
+
+
+#endif //CPP_MPC_PAILLIAR_H
---
+++ src/crypto-paillier/pail_pubkey.h
@@ -0,0 +1,112 @@
+//
+// Created by 何剑虹 on 2021/6/9.
+//
+
+#ifndef CPP_MPC_PAIL_PUBKEY_H
+#define CPP_MPC_PAIL_PUBKEY_H
+
+#include <string>
+#include "crypto-bn/bn.h"
+#include "paillier.pb.h"
+
+namespace safeheron{
+namespace pail {
+
+class PailPrivKey;
+
+class PailPubKey {
+    friend void CreateKeyPair(PailPrivKey &priv, PailPubKey &pub, int key_bits);
+
+public:
+    PailPubKey();
+
+    /**
+     * Constructor of PailPubKey
+     * @param n = pq
+     * @param g = n + 1
+     * @constructor
+     */
+    PailPubKey(const safeheron::bignum::BN &n, const safeheron::bignum::BN &g);
+
+    std::string Inspect() const;
+
+    /**
+     * Encrypt:
+     *     c = g^m * r^n mod n^2
+     *       = (1 + m*n) * r^n mod n^2
+     *
+     * @param {safeheron::bignum::BN} m: number to be encrypted
+     * @param {safeheron::bignum::BN} r : random number
+     */
+    safeheron::bignum::BN EncryptWithR(const safeheron::bignum::BN &m, const safeheron::bignum::BN &r) const;
+
+    safeheron::bignum::BN EncryptWithR_v0(const safeheron::bignum::BN &m, const safeheron::bignum::BN &r) const;
+
+    /**
+     * Encrypt:
+     *     c = g^m * r^n mod n^2
+     *       = (1 + m*n) * r^n mod n^2
+     *
+     * @param {safeheron::bignum::BN} m: number to be encrypted
+     */
+    safeheron::bignum::BN Encrypt(const safeheron::bignum::BN &m) const;
+
+    safeheron::bignum::BN Encrypt_v0(const safeheron::bignum::BN &m) const;
+
+    /**
+     * Homomorphic add:
+     *     E(a+b) = E(a) * E(b) mod n^2
+     * @param {safeheron::bignum::BN} e_a: encrypted num a
+     * @param {safeheron::bignum::BN} e_b: encrypted num b
+     */
+    safeheron::bignum::BN Add(const safeheron::bignum::BN &e_a, const safeheron::bignum::BN &e_b) const;
+
+    /**
+     * Homomorphic add:
+     *     E(a+b) = E(a) * g^b mod n^2
+     *            = E(a) * (1 + b*n) mod n^2
+     * @param {safeheron::bignum::BN} e_a: encrypted num a
+     * @param {safeheron::bignum::BN} b: plain num b
+     */
+    safeheron::bignum::BN AddPlain(const safeheron::bignum::BN &e_a, const safeheron::bignum::BN &b) const;
+
+    safeheron::bignum::BN AddPlain_v0(const safeheron::bignum::BN &e_a, const safeheron::bignum::BN &b) const;
+
+    /**
+     * Homomorphic multiple:
+     *     E(ka) = E(a) ^ k mod n^2
+     * @param {safeheron::bignum::BN} e_a: encrypted num a
+     * @param {safeheron::bignum::BN} k: plain num to multiple
+     */
+    safeheron::bignum::BN Mul(const safeheron::bignum::BN &e_a, const safeheron::bignum::BN &k) const;
+
+    safeheron::bignum::BN n() const { return n_; }
+
+    safeheron::bignum::BN g() const { return g_; }
+
+    safeheron::bignum::BN n_sqr() const { return n_sqr_; }
+
+    bool ToProtoObject(proto_basic::PailPub &pail_pub) const;
+
+    bool FromProtoObject(const proto_basic::PailPub &pail_pub);
+
+    bool ToBase64(std::string &base64) const;
+
+    bool FromBase64(const std::string &base64);
+
+    bool ToJsonString(std::string &json_str) const;
+
+    bool FromJsonString(const std::string &json_str);
+
+private:
+    safeheron::bignum::BN n_;   // n = pq
+    safeheron::bignum::BN g_;   // g = n + 1
+    safeheron::bignum::BN n_sqr_;
+
+};
+
+};
+};
+
+
+#endif //CPP_MPC_PAIL_PUBKEY_H
